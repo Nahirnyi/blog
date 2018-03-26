d60d27296231a3f3aed898774a1a3f848aa2ce2f
Nahirnyi <nahirnujjaroslaw@gmail.com>
2018-03-27 01:58:14 +0300

    9

--- routes/web.php
+++ routes/web.php
@@ -11,16 +11,5 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/tasks', function () {
-    $tasks = DB::table('tasks')->latest()->get();
-    return view('tasks.index', compact('tasks'));
-});
-
-Route::get('/tasks/{task}', function ($id) {
-    $task = DB::table('tasks')->find($id);
-    return view('tasks.show', compact('task'));
-});
+Route::get('/tasks', 'TasksController@index');
+Route::get('/tasks/{task}', 'TasksController@show');
