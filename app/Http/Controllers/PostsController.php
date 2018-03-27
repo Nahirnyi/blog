5943ab79d255690dd0e2b9981df18dc83ce5ea4f
Nahirnyi <nahirnujjaroslaw@gmail.com>
2018-03-27 16:59:11 +0300

    13

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -8,12 +8,14 @@ class PostsController extends Controller
 {
     public function index()
     {
-        return view('posts.index');
+        $posts = Post::latest()->get();
+
+        return view('posts.index', compact('posts'));
     }
 
-    public function show()
+    public function show(Post $post)
     {
-        return view('posts.show');
+        return view('posts.show', compact('post'));
     }
 
     public function create()
@@ -23,6 +25,10 @@ class PostsController extends Controller
 
     public function store()
     {
+        $this->validate(request(),[
+            'title' => 'required',
+            'body' => 'required'
+        ]);
         Post::create(request(['title', 'body']));
         return redirect('/');
     }
