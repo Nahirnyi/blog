7120836cb76cd7700542cdcd919506ac45e6a207
Nahirnyi <nahirnujjaroslaw@gmail.com>
2018-03-27 18:18:42 +0300

    Aded Comments

--- app/Post.php
+++ app/Post.php
@@ -5,5 +5,13 @@ namespace App;
 
 class Post extends Model
 {
-    protected $fillable = ['title', 'body'];
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
+
+    public function addComment($body)
+    {
+        $this.$this->comments()->create(compact('body'));
+    }
 }
--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -8,11 +8,11 @@
             {{ csrf_field() }}
             <div class="form-group">
                 <label for="title">Title: </label>
-                <input type="text" class="form-control" id="title" name="title">
+                <input type="text" class="form-control" id="title" name="title" required>
             </div>
             <div class="form-group">
                 <label for="body">Body: </label>
-                <textarea name="body" id="body" class="form-control"></textarea>
+                <textarea name="body" id="body" class="form-control" required></textarea>
             </div>
             <div class="form-group">
                 <button type="submit" class="btn btn-primary">Publish</button>
