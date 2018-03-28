7505b7e86ac99936b90c5ca123f0b42aa36ae5b6
Nahirnyi <nahirnujjaroslaw@gmail.com>
2018-03-28 10:40:24 +0300

    Aded FormRequest

--- app/Http/Controllers/RegistrationController.php
+++ app/Http/Controllers/RegistrationController.php
@@ -2,8 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Mail\Welcome;
-use App\User;
+use App\Http\Requests\RegistrationForm;
 
 class RegistrationController extends Controller
 {
@@ -12,23 +11,9 @@ class RegistrationController extends Controller
         return view('registration.create');
     }
 
-    public function store()
+    public function store(RegistrationForm $request)
     {
-        $this->validate(request(),[
-           'name' => 'required',
-           'email' => 'required|email|unique:users',
-           'password' => 'required'
-        ]);
-
-        $user = User::create([
-            'name' => request('name'),
-            'email' => request('email'),
-            'password' => bcrypt(request('password'))
-        ]);
-
-        auth()->login($user);
-
-        \Mail::to($user)->send(new Welcome($user));
+        $request->persist();
 
         return redirect()->home();
     }
